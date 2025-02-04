d99aed2300829e7c90c339393cee3f3a8d6a037e
Haley Goodfield <haleygoodfield@usf.edu>
2025-02-04 10:09:51 -0500

    Task 1 - Customer Profile

---
+++ cc_5.js
@@ -0,0 +1,9 @@
+// Task 1: Object Properties
+// Declare an object customer with the properties
+const customer = { name: "Noelle Fedorko", age: 20, email: "noellef@gmail.com"}; 
+
+// Log each property using console.log() and a template literal.
+console.log(`Customer's name: ${customer.name}`);
+console.log(`Customer's Age: ${customer.age}`);
+console.log(`Customer's Email: ${customer.email}`);
+
